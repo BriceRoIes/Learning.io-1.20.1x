b9f971a2b2d5ec01c9052b047152860fc1e81da6
BriceRoIes <wardo619@gmail.com>
2023-12-29 13:56:15 -0800

    Initial commit

---
+++ src/main/java/net/briceroles/learningio/LearningIOClient.java
@@ -0,0 +1,10 @@
+package net.briceroles.learningio;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LearningIOClient implements  ClientModInitializer {
+    @Override
+    public  void onInitializeClient() {
+
+    }
+}
